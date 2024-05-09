91c11907cbc207fbda4dd784b96447379e7fa007
Jissy96 <jissyj1996@gmail.com>
2024-05-08 21:58:10 -0400

    Initializing CookieCutter Data Science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Smart Parking Solution is an innovative project aimed at optimizing parking management by integrating live camera streams from parking lots to detect empty spaces and count available spots.',
+    author='AISC-Spot-Finder',
+    license='MIT',
+)
