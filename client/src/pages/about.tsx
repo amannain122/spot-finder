beff511c5964e21277dab65c09b9d4e80a64822d
Prakash Pun <poonprakash22@gmail.com>
2024-05-13 18:16:58 -0400

    fix: remove unsued code

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const AboutPage = () => {
   return <div>AboutPage</div>;
 };
--- client/src/pages/team.tsx
+++ client/src/pages/team.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const TeamPage = () => {
   return <div>TeamPage</div>;
 };
