bb7434ae475142069bfe097419bfdeb1e6b96ac5
Prakash Pun <poonprakash22@gmail.com>
2024-05-20 11:29:06 -0400

    fix update config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,13 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-  },
-  preview: {
-    port: 4000,
-  },
+
   build: {
     outDir: "./dist",
   },
