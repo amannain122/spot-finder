2088e5d6d77467d4d547ccd0f8f4b72ae63ae881
Prakash Pun <poonprakash22@gmail.com>
2024-06-26 13:24:51 -0400

    test using aws redshift_connector

--- server/test.py
+++ server/test.py
@@ -1,52 +1,53 @@
 
+import redshift_connector
 import psycopg2
-# import os
-# import pandas as pd
+# # import os
+# # import pandas as pd
 
-# try:
-#     from sqlalchemy import create_engine
-# except ModuleNotFoundError:
+# # try:
+# #     from sqlalchemy import create_engine
+# # except ModuleNotFoundError:
 
-#     from sqlalchemy import create_engine
-# try:
-#     import psycopg2
-# except ModuleNotFoundError:
-#     import psycopg2
+# #     from sqlalchemy import create_engine
+# # try:
+# #     import psycopg2
+# # except ModuleNotFoundError:
+# #     import psycopg2
 
-# print('Libraries imported successfully')
+# # print('Libraries imported successfully')
 
 
-# # create sql engine to connect to redshift
+# # # create sql engine to connect to redshift
 
-# engine_string = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(
-#     "awsuser", "Anjitha97", "redshift-cluster-1.cqyedsmaziep.us-east-2.redshift.amazonaws.com", "5439", "dev")
-# engine = create_engine(engine_string)
+# # engine_string = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+# #     "awsuser", "Anjitha97", "redshift-cluster-1.cqyedsmaziep.us-east-2.redshift.amazonaws.com", "5439", "dev")
+# # engine = create_engine(engine_string)
 
-# # redshift-cluster-1.cqyedsmaziep.us-east-2.redshift.amazonaws.com:5439/dev
+# # # redshift-cluster-1.cqyedsmaziep.us-east-2.redshift.amazonaws.com:5439/dev
 
-# print('Engine created successfully')
+# # print('Engine created successfully')
 
-# print(engine)
-# # change the query as needed. <table> should be the database name and the table name.
+# # print(engine)
+# # # change the query as needed. <table> should be the database name and the table name.
 
-# sql = """
-#     SELECT *
-#     FROM <table>
-#     LIMIT 5
-# """
+# # sql = """
+# #     SELECT *
+# #     FROM <table>
+# #     LIMIT 5
+# # """
 
-# print(sql)
+# # print(sql)
 
-# df = pd.read_sql(sql, engine)
-# # df
+# # df = pd.read_sql(sql, engine)
+# # # df
 
 
 # Connection parameters
-dbname = 'your_database_name'
+dbname = 'dev'
 user = 'awswer'
 password = 'Anjitha97'
 host = "redshift-cluster-1.cqyedsmaziep.us-east-2.redshift.amazonaws.com"
-port = '5430'
+port = '5439'
 
 # Establish connection
 conn = psycopg2.connect(dbname=dbname, user=user,
@@ -63,3 +64,25 @@ print(rows)
 # Close cursor and connection
 cur.close()
 conn.close()
+
+
+# Connects to Redshift cluster using AWS credentials
+conn = redshift_connector.connect(
+    host='redshift-cluster-1.cqyedsmaziep.us-east-2.redshift.amazonaws.com',
+    database='dev',
+    user='awsuser',
+    password='Anjitha97'
+)
+
+cursor: redshift_connector.Cursor = conn.cursor()
+cursor.execute("create Temp table parking(bookname varchar,author varchar)")
+cursor.executemany("insert into book (bookname, author) values (%s, %s)",
+                   [
+                       ('One Hundred Years of Solitude', 'Gabriel García Márquez'),
+                       ('A Brief History of Time', 'Stephen Hawking')
+                   ]
+                   )
+cursor.execute("select * from book")
+
+result: tuple = cursor.fetchall()
+print(result)
