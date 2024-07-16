d0b9af3480c615df30af46394a53fe3ee076ab42
JISNA D KUNJU <156552587+7994581443@users.noreply.github.com>
2024-07-16 15:08:15 -0400

    update qr

--- client/src/component/QRCodeComponent.tsx
+++ client/src/component/QRCodeComponent.tsx
@@ -1,43 +1,18 @@
-// // src/component/QRCodeComponent.tsx
-// import React from 'react';
-// import QRCode from 'qrcode.react';
-
-// interface QRCodeComponentProps {
-//   url: string;
-// }
-
-// const QRCodeComponent: React.FC<QRCodeComponentProps> = ({ url }) => {
-//   return (
-//     <div>
-//       <QRCode value={url} />
-//     </div>
-//   );
-// };
-
-// export default QRCodeComponent;
-// src/components/QRCodeComponent.tsx
+"use client";
+// src/component/QRCodeComponent.tsx
 import React from 'react';
 import QRCode from 'qrcode.react';
 
 interface QRCodeComponentProps {
-  value: string; // Renamed prop to match 'value' instead of 'url' for clarity
-  size?: number;
-  bgColor?: string;
-  fgColor?: string;
+  url: string;
 }
 
-const QRCodeComponent: React.FC<QRCodeComponentProps> = ({
-  value,
-  size = 128, // Default size if not provided
-  bgColor = '#FFFFFF', // Default background color
-  fgColor = '#000000' // Default foreground color
-}) => {
+const QRCodeComponent: React.FC<QRCodeComponentProps> = ({ url }) => {
   return (
     <div>
-      <QRCode value={value} size={size} bgColor={bgColor} fgColor={fgColor} />
+      <QRCode value={url} />
     </div>
   );
 };
 
 export default QRCodeComponent;
-
