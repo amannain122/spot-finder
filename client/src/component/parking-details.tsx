48beee4cc55d2e7b879dc5a73afb7e45a69d4be2
JISNA D KUNJU <156552587+7994581443@users.noreply.github.com>
2024-07-21 14:53:03 -0400

    Qr confirmation page created

--- client/src/component/parking-details.tsx
+++ client/src/component/parking-details.tsx
@@ -1,8 +1,8 @@
- "use client";
+"use client";
 
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { MapDetail } from "./map-detail";
-import QRCodeComponent from "@/component/QRCodeComponent"; 
+import QRCodeComponent from "@/component/QRCodeComponent";
 
 const parkingLots = [
   {
@@ -32,18 +32,21 @@ const parkingLots = [
 ];
 
 const ParkingDetail = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const search = searchParams.get("id");
   const parkingDtl: any = parkingLots.find((data: any) => data.id === search);
 
   const qrCodeUrl = `http://localhost:3000/parking-detail?id=${search}`;
 
-  console.log(search, parkingDtl);
+  const handleConfirmSelection = () => {
+    router.push(`/qrconfirmation?id=${search}&address=${encodeURIComponent(parkingDtl.address)}&availability=${parkingDtl.availability}&rating=${parkingDtl.rating}`);
+  };
+
   return (
     <div>
       <div className="flex flex-row items-center justify-between w-full px-4 gap-8 mt-8">
-        {/* Add the Discover Affordable Parking Effortlessly with Spot Finder */}
-        <div className=" p-4">
+        <div className="p-4">
           <h2 className="text-lg font-semibold mb-2">{parkingDtl.address}</h2>
           <div className="flex justify-between items-center mb-2">
             <span>Availability - {parkingDtl.availability}</span>
@@ -52,17 +55,19 @@ const ParkingDetail = () => {
             Rating: <span>{parkingDtl.rating.toFixed(1)}</span>
           </div>
           <br />
-          <button className="bg-white text-gray-700 py-2 px-4 rounded border shadow-lg border-gray-300">
+          <button
+            className="bg-white text-gray-700 py-2 px-4 rounded border shadow-lg border-gray-300"
+            onClick={handleConfirmSelection}
+          >
             Confirm Selection
           </button>
         </div>
 
-        {/* Add the Ground Floor text */}
         <div className="flex-grow">
           <img
             src={parkingDtl.image}
             alt="Parking Lot"
-            className=" w-full h-full object-cover rounded-lg border-2"
+            className="w-full h-full object-cover rounded-lg border-2"
             style={{ width: "500px", height: "200px" }}
           />
         </div>
