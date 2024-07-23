ae6435bac5a73ffaf9d93cc054f2cc7bb3caf9e7
Bibek Ranjit <bibek.ranjit@gmail.com>
2024-07-22 20:25:31 -0400

    Script for uploading parking status csv to s3 added

---
+++ notebooks/Yolo-V8/check_and_upload_parking_status.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the name of the process or script to check
+PROCESS_NAME="statusupdate.py"
+
+# Define the full path to your script
+SCRIPT_PATH="/home/ubuntu/spot-finder/notebooks/Yolo-V8/statusupdate.py"
+
+LOG_FILE="/home/ubuntu/spot-finder/notebooks/Yolo-V8/logs/upload_status.log"
+
+source /home/ubuntu/spot-finder/venv/bin/activate
+
+python3 "$SCRIPT_PATH" >> "$LOG_FILE" 2>&1 &
+
+deactivate
