aad53f1138df407ab41429f10c140117503340fe
Bibek Ranjit <bibek.ranjit@gmail.com>
2024-07-24 18:04:42 -0400

    Added script for uploading fare_calculated csv to s3

---
+++ experimental/pipeline/scripts/upload_fare_to_s3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the name of the process or script to check
+PROCESS_NAME="data_uploader.py"
+
+# Define the full path to your script
+SCRIPT_PATH="/home/ubuntu/spot-finder/experimental/pipeline/model-related/data_uploader.py"
+
+LOG_FILE="/home/ubuntu/spot-finder/experimental/pipeline/model-related/logs/data_uploader_status.log"
+
+source /home/ubuntu/spot-finder/venv/bin/activate
+
+python3 "$SCRIPT_PATH" >> "$LOG_FILE" 2>&1 &
+
+deactivate
