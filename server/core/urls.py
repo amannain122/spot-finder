c5c06429e9f27de4202d321ec1893d4b20e19208
Prakash Pun <poonprakash22@gmail.com>
2024-08-03 01:04:19 -0400

    update settings.py

--- server/core/urls.py
+++ server/core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls', namespace='api'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
